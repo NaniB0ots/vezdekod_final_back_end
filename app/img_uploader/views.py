6fce96552bff661bd6af6c1520d2bac68dcf2d40
Алексей <alexleskov2000@gmail.com>
2021-11-04 18:40:06 +0800

    Загрузка файла

--- app/img_uploader/views.py
+++ app/img_uploader/views.py
@@ -8,11 +8,10 @@ from img_uploader.serializers import ImageSerializer
 class UploadImageView(APIView):
 
     def post(self, request, format=None):
-        print('Yes')
         serializer = ImageSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            instance = serializer.save()
+            return Response(instance.id, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
