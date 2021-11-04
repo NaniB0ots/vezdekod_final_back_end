b4324c612e0214d2a80c50c431868fdd55a912b7
Алексей <alexleskov2000@gmail.com>
2021-11-05 04:38:32 +0800

    Определение "похожести изображений"

--- app/img_uploader/models.py
+++ app/img_uploader/models.py
@@ -1,13 +1,20 @@
 import uuid
 
+import PIL
+import imagehash
 from django.db import models
 
 
 class Image(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     file = models.ImageField('Изображение', upload_to='%Y/%m/%d/')
-    # height = models.FloatField('Высота')
-    # width = models.FloatField('Ширина')
+    p_hash = models.CharField(max_length=64, blank=True)
 
     creation_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.p_hash:
+            image = PIL.Image.open(self.file)
+            self.p_hash = imagehash.whash(image).hash.tostring().decode('UTF-8')
+        return super(Image, self).save(*args, **kwargs)
--- app/img_uploader/views.py
+++ app/img_uploader/views.py
@@ -10,6 +10,8 @@ from img_uploader.serializers import ImageSerializer
 from . import models
 
 from PIL import Image
+import imagehash
+import numpy as np
 
 
 class UploadImageView(APIView):
@@ -25,11 +27,45 @@ class UploadImageView(APIView):
 
 class ShowImageView(APIView):
     def get(self, request, pk: str):
+
+        # sql = 'SELECT *, BIT_COUNT(0xfefefed3e0a02000 ^ p_hash) as hamming_distance ' \
+        #       'FROM img_uploader_image ' \
+        #       'HAVING hamming_distance <= 10'
+
         try:
             instance = models.Image.objects.get(pk=pk)
         except (models.Image.DoesNotExist, ValidationError):
             return Response('Изображение не найдено', status=status.HTTP_404_NOT_FOUND)
         image: Image = Image.open(instance.file)
+
+        image_hash = imagehash.whash(image)
+
+        ratio = image.height / image.width
+        print('ratio: ', ratio)
+        for item in models.Image.objects.exclude(id=pk):
+
+            bit_array = []
+            bit_row = []
+            i = 0
+            for bit in item.p_hash.encode('utf-8'):
+                i += 1
+                bit_row.append(bit)
+                if i == 8:
+                    bit_array.append(bit_row)
+                    bit_row = []
+                    i = 0
+            bit_array = np.array(bit_array)
+
+            h_distance = (imagehash.ImageHash(bit_array) - image_hash)
+            print('h_distance: ', h_distance)
+            print(item)
+
+
+            item_image = Image.open(item.file)
+            ratio_item = item_image.height / item_image.width
+            print('ratio_item: ', ratio_item)
+            if h_distance < 10:
+                pass
 
         # масштабирование
         scale = request.GET.get('scale')
